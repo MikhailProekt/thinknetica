aa4d33d390c53659dc2bfbd117389d0a410cdd5c
Mikhail Proekt <proect24@mail.ru>
2018-11-17 13:45:21 +0300

    Last changes

--- lesson1/ex3.rb
+++ lesson1/ex3.rb
@@ -5,14 +5,14 @@ puts "Введите сторону треугольника b:"
 sides_triangle << gets.chomp.to_f
 puts "Введите сторону треугольника c:"
 sides_triangle << gets.chomp.to_f
-sides_triangle.sort!
-if sides_triangle[0] ** 2 + sides_triangle[1] ** 2 == sides_triangle[2] ** 2
+a, b, c = sides_triangle.sort!
+if a**2 + b**2 == c**2
   puts "Ваш треугольник прямоугольный"
 else 
   puts "Ваш треугольник не прямоугольный"
 end
-if sides_triangle[0] == sides_triangle[1] && sides_triangle[1] == sides_triangle[2]
+if a == b && b == c
   puts "Ваш треугольник равнобедренный и равносторонний, но не прямоугольный"
-elsif sides_triangle[0] == sides_triangle[1] || sides_triangle[0] == sides_triangle[2] || sides_triangle[1] == sides_triangle[2]
+elsif a == b || a == c || b == c
   puts "Ваш треугольник равнобедренный"
 end
